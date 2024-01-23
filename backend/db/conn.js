6d64fe3c1bd76a9ae6727c8c76557728acd5b578
Pietro Mizael <pietromizael@gmail.com>
2024-01-23 16:27:25 -0300

    Att backend

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config( {path: "db/variables.env"} );
+
+const key = process.env.MONGO_KEY;
 
 async function connect() {
-    try {
-        await mongoose.connect('mongodb+srv://pietromizael:kVDOHc5rIDB5naPT@cluster0.kvkvahc.mongodb.net')
-        console.log("Conectado ao MongoDB")
-    } catch (error) {
-        console.log("Error connecting to MongoDB");
-    }
+  try {
+    await mongoose.connect(
+      `mongodb+srv://pietromizael:${key}naPT@cluster0.kvkvahc.mongodb.net`
+    );
+    console.log("Conectado ao MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB");
+  }
 }
 
 module.exports = connect;
