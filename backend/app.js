13f744ce413cb680337bc8037fba8470a20f3131
EC2 Default User <ec2-user@ip-172-31-40-133.ec2.internal>
2024-01-24 03:06:57 +0000

    app

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(contactRoutes);
+app.get('/', (req,res) => {
+res.send('Bem vindo ao meu servidor!');
+});
 
 const conn = require('./db/conn.js');
 const Contact = require('./models/Contact.js');
@@ -13,7 +16,7 @@ conn();
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, '0.0.0.0', () => {
+app.listen(3001, '0.0.0.0', () => {
     console.log('Server on');
 });
 
